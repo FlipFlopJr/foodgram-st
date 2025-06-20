7084ea34be7d5ec77e2a7f6b4654ba752a8bd355
Andrey Lavrinovich <lavrinovitch.andreas@yandex.ru>
2025-06-20 20:31:13 +0300

    change filters.py

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,38 +1,36 @@
 from django_filters.rest_framework import FilterSet, filters
-
 from recipes.models import IngredientModel, RecipeModel
 
 
 class FilterIngredientModel(FilterSet):
-    """Фильтр для модели ингридиентов"""
+    """Фильтр для модели ингредиентов"""
 
     name = filters.CharFilter(field_name="name", lookup_expr="istartswith")
 
     class Meta:
         model = IngredientModel
-        fields = ["name", ]
+        fields = ["name"]
 
 
 class FilterRecipeModel(FilterSet):
     """Фильтр для модели рецептов"""
+
     author = filters.NumberFilter(field_name="author__id")
-    shopping_cart = filters.BooleanFilter(
-        method="filter_of_shopping_cart"
-    )
+    shopping_cart = filters.BooleanFilter(method="filter_shopping_cart")
     is_favorited = filters.BooleanFilter(method="filter_is_favorited")
 
     class Meta:
         model = RecipeModel
         fields = ["author", "is_favorited", "shopping_cart"]
 
-    def filter_is_favorited(self, recipes, name, value):
-        current_user = self.request.user
-        if current_user.is_authenticated and value:
-            return recipes.filter(favoriterecipemodel_relations__user=current_user)
-        return recipes
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if user.is_authenticated and value:
+            return queryset.filter(favoriterecipemodel_relations__user=user)
+        return queryset
 
-    def filter_of_shopping_cart(self, recipes, name, value):
-        current_user = self.request.user
-        if current_user.is_authenticated and value:
-            return recipes.filter(shoppingcart_relations__user=current_user)
-        return recipes
+    def filter_of_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if user.is_authenticated and value:
+            return queryset.filter(shoppingcart_relations__user=user)
+        return queryset
